cfc1bdcd7fdb81a5914e99b86578245c765901ae
sammy 33 <sammy@MoonScape.Home>
2025-12-18 06:32:36 +0000

    Final Commit for today, added clarity to the code comments and retested deployment, start and stop bash scripts are slightly tweaked also

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script lets me quickly deploy my
+
+gcloud builds submit .
--- stop.sh
+++ stop.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+
+#Easy way to kill my current build for testing.
+
 gcloud run services delete markdown-editor \
   --platform managed \
   --region us-central1 \
